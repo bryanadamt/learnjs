9e63a9c2f6374e84a3ab138fb1d562f1e0539813
Howsan <tjendrabryan@gmail.com>
2022-04-18 17:34:11 -0700

    yo the express api is up wow

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const app = require("express")();
+const PORT = 8080;
+
+app.listen(PORT, () => console.log("Running on http://localhost:${PORT}"));
+
+app.get("/info", (req, res) => {
+    res.status(200).send({
+        name: "first API with express",
+        created: "18 April 22",
+    });
+});
