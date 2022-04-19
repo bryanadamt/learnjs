ea8694b5b805094800f39eca00c5df57fc54ed1e
Howsan <tjendrabryan@gmail.com>
2022-04-18 19:18:35 -0700

    changed theme and modified it plus added more endpoints

--- index.js
+++ index.js
@@ -9,3 +9,30 @@ app.get("/info", (req, res) => {
         encouragement: "the first step is pretty hard",
     });
 });
+
+app.get("/work", (req, res) => {
+    res.status(200).send({
+        work: "software engineering",
+        time: "5am - drop",
+        salary: "peanuts",
+    });
+});
+
+app.get("/trial", (req, res) => {
+    res.status(200).send({
+        trial:"complete",
+        success:"not guaranteed"
+    });
+});
+
+app.get("/hack", (req, res) => {
+    res.status(200).send({
+        hack:"initiated",
+        times:42,
+        NSA:"gone"
+    });
+});
+
+app.post("info/:id", (req, res) => {
+    const { id } = req.params;
+});
